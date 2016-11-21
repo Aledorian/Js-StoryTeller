6bd0fdf5ff5cf03802f09fa7b7317472b181561e
simplon <aledorianlegrand@gmail.com>
2016-11-21 15:40:49 +0100

    It's over

--- js/Js-StoryTeller.js
+++ js/Js-StoryTeller.js
@@ -1,5 +1,5 @@
+console.log("Début du script");
 var prenom = prompt("Entrez un prénom.");
-
 var compagnon = prompt ("Entrez deux autres prénoms séparé par un 'et'");
 var classe = prompt("Entrez une classe d'héroïque fantasy.");
 var monstre = prompt("Entrez le nom commun d'une créature fantastique");
@@ -7,6 +7,7 @@ var membre = prompt("Entrez un membre du corps humain.");
 var splCompagnon = compagnon.split("et");
 var compagnon1 = splCompagnon[0];
 var compagnon2 = splCompagnon[1];
+
 document.getElementById('debut').style.animationPlayState="running"
 document.getElementById('milieu').style.animationPlayState="running"
 document.getElementById('fin').style.animationPlayState="running"
@@ -20,4 +21,9 @@ document.getElementsByClassName('comp2')[0].innerHTML = compagnon2;
 document.getElementsByClassName('comp2')[1].innerHTML = compagnon2;
 document.getElementsByClassName('comp2')[2].innerHTML = compagnon2;
 
-console.log("Prenom = " + prenom, "Compagnon = " + compagnon, "Classe = " + classe, "Monstre = " + monstre, "Membre = " + membre);
+console.log("Prenom = " + prenom);
+console.log("Compagnon = " + compagnon);
+console.log("Classe = " + classe);
+console.log("Monstre = " + monstre);
+console.log("Membre = " + membre);
+console.log("fin du script");
