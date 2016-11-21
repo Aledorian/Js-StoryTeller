8d54189799c81d1835b8644d95d2328bbe0741c2
simplon <aledorianlegrand@gmail.com>
2016-11-21 11:38:02 +0100

    Histoire finie

--- js/Js-StoryTeller.js
+++ js/Js-StoryTeller.js
@@ -1,13 +1,18 @@
-var prenom = prompt("Entrer un prénom.");
-var compagnon = prompt ("Entrer deux autres prénoms séparé par un 'et'");
-var classe = prompt("Entrer une classe d'héroïque fantasy.");
-var monstre = prompt("Entrer le nom commun d'une créature fantastique");
-var membre = prompt("Entre un membre du corps humain.");
-var splCompagnon = compagnon.split("et")
-console.log(splCompagnon);
+var prenom = "Elizra";//prompt("Entrez un prénom.");
+var compagnon = "Alastasia et Aladenor";//prompt ("Entrez deux autres prénoms séparé par un 'et'");
+var classe = "voleur";//prompt("Entrez une classe d'héroïque fantasy.");
+var monstre = "vampire"//prompt("Entrez le nom commun d'une créature fantastique");
+var membre = "jambe gauche"//prompt("Entrez un membre du corps humain.");
+var splCompagnon = compagnon.split("et");
+var compagnon1 = splCompagnon[0];
+var compagnon2 = splCompagnon[1];
 
-document.getElementById('principal').innerHTML = prenom
-document.getElementById('amis').innerHTML = compagnon
-document.getElementById('creature').innerHTML = monstre
-document.getElementById('metier').innerHTML = classe
-document.getElementById('blessure').innerHTML = membre
+document.getElementById('principal').innerHTML = prenom;
+document.getElementById('amis').innerHTML = compagnon;
+document.getElementById('creature').innerHTML = monstre;
+document.getElementById('metier').innerHTML = classe;
+document.getElementById('blessure').innerHTML = membre;
+document.getElementById('comp1').innerHTML = compagnon1;
+document.getElementsByClassName('comp2')[0].innerHTML = compagnon2;
+document.getElementsByClassName('comp2')[1].innerHTML = compagnon2;
+document.getElementsByClassName('comp2')[2].innerHTML = compagnon2;
