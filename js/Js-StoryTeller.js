8e4338f6dffd66a72684e1f699f16b79defff61e
simplon <aledorianlegrand@gmail.com>
2016-11-21 15:12:57 +0100

    animation

--- js/Js-StoryTeller.js
+++ js/Js-StoryTeller.js
@@ -1,4 +1,5 @@
 var prenom = prompt("Entrez un prénom.");
+
 var compagnon = prompt ("Entrez deux autres prénoms séparé par un 'et'");
 var classe = prompt("Entrez une classe d'héroïque fantasy.");
 var monstre = prompt("Entrez le nom commun d'une créature fantastique");
@@ -6,7 +7,9 @@ var membre = prompt("Entrez un membre du corps humain.");
 var splCompagnon = compagnon.split("et");
 var compagnon1 = splCompagnon[0];
 var compagnon2 = splCompagnon[1];
-
+document.getElementById('debut').style.animationPlayState="running"
+document.getElementById('milieu').style.animationPlayState="running"
+document.getElementById('fin').style.animationPlayState="running"
 document.getElementById('principal').innerHTML = prenom;
 document.getElementById('amis').innerHTML = compagnon;
 document.getElementById('creature').innerHTML = monstre;
@@ -17,4 +20,4 @@ document.getElementsByClassName('comp2')[0].innerHTML = compagnon2;
 document.getElementsByClassName('comp2')[1].innerHTML = compagnon2;
 document.getElementsByClassName('comp2')[2].innerHTML = compagnon2;
 
-console.log("Prenom = " + prenom, "Compagnon = " + compagnon, "Classe = " + classe, "Monstre = " + monstre, "Membre = " + membre, "Compagnon1 = " + compagnon1, "Compagnon2 = " + compagnon2);
+console.log("Prenom = " + prenom, "Compagnon = " + compagnon, "Classe = " + classe, "Monstre = " + monstre, "Membre = " + membre);
